91ee62d31c67d2dbf9df5bd6550ba4a3f0192928
spikespiegel <583288277@qq.com>
2024-12-03 12:33:45 +0800

    temp

--- src/components.d.ts
+++ src/components.d.ts
@@ -22,6 +22,7 @@ declare module 'vue' {
     AModal: typeof import('ant-design-vue/es')['Modal']
     ARow: typeof import('ant-design-vue/es')['Row']
     ASelect: typeof import('ant-design-vue/es')['Select']
+    ASelectOption: typeof import('ant-design-vue/es')['SelectOption']
     ASpace: typeof import('ant-design-vue/es')['Space']
     ATable: typeof import('ant-design-vue/es')['Table']
     ATableColumn: typeof import('ant-design-vue/es')['TableColumn']
--- src/views/Dashboard/Right/Right.vue
+++ src/views/Dashboard/Right/Right.vue
@@ -6,22 +6,26 @@
           <a-col :span="12"> </a-col>
           <a-col :span="12">
             <a-form-item label="区域">
-              <a-select></a-select>
+              <a-select>
+                <a-select-option value="全部">全部</a-select-option>
+              </a-select>
             </a-form-item>
           </a-col>
         </a-row>
       </a-form>
     </div>
     <div class="content">
-      <a-space wrap>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </a-space>
+      <vue-scroll>
+        <a-space wrap>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </a-space>
+      </vue-scroll>
     </div>
   </div>
 </template>
@@ -68,6 +72,7 @@ onBeforeUnmount(() => {});
       width: 100%;
       .ant-space-item {
         border: 1px solid #ccc;
+
         > div {
           width: 3rem;
           height: 3rem;
