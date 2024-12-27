3b4f1b833df87be7e669e88015402a25bf1698d4
Darren <darren0801@163.com>
2024-12-27 19:57:16 +0800

    视频盖住其他元素-只显示4个

--- src/views/Dashboard/Right/Right.vue
+++ src/views/Dashboard/Right/Right.vue
@@ -20,8 +20,10 @@
           </a-col>
           <a-col :span="12">
             <a-form-item label="区域">
-              <a-select placeholder="请选择">
+              <a-select v-model:value="selectedRegion" placeholder="请选择" @change="handleSelectChange" style="z-index: 1000;">
                 <a-select-option value="全部">全部</a-select-option>
+                <!-- <a-select-option value="BM1">BM1</a-select-option>
+                <a-select-option value="BM2">BM2</a-select-option> -->
               </a-select>
             </a-form-item>
           </a-col>
@@ -30,11 +32,12 @@
     </div>
 
     <div class="content">
-      <vue-scroll>
-        <a-space wrap>
-          <div id="videoDOM"></div>
-        </a-space>
-      </vue-scroll>
+      <div id="videoDOM"></div>
+      <div class="mapPic">
+        <img
+          src="@/assets/mapPic.png"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -63,19 +66,21 @@ const global = currentInstance.appContext.config.globalProperties;
 
 const formDataRef = ref();
 const videoDomId = ref();
-const searchRecordsData = ref();
+const AccessToken = ref();
+const cameraListTotal = ref();
+const cameraListChecked = ref();
+const selectedRegion = ref('全部');
 const isActive = ref("p9");
 
 const state = reactive({});
 
 // 定义 winContent 响应式数据
 const winContent = ref("");
+const defaultCameraList = ref("");
 
 onMounted(async () => {
-  // 登录
   try {
     const res1 = await axios.post("http://10.141.10.10:8088/VIID/login/v2", {});
-    console.log("res1  ", res1);
     const postData = {
       UserName: "loadmin",
       AccessCode: res1.data.AccessCode,
@@ -86,14 +91,12 @@ onMounted(async () => {
       ).toString(),
     };
 
-    // 二次调用获取token
     const res2 = await axios.post(
       "http://10.141.10.10:8088/VIID/login/v2",
       postData
     );
-    console.log("res2", res2, btoa("loadmin"));
+    AccessToken.value = res2.data.AccessToken;
 
-    // 鉴权+keeplive
     res2.data.AccessToken
       ? setInterval(async () => {
           await axios.post(
@@ -108,30 +111,31 @@ onMounted(async () => {
         }, 30000)
       : null;
 
-    // 动态加载外部脚本
     await loadScript();
-    // 初始化
     const initData = {
       ip: "10.141.10.10",
-      token: res2.data.AccessToken,
+      token: AccessToken.value,
       title: document.title,
       offset: [0, 0],
     };
-    console.log("initData", initData);
 
     await init(initData);
 
     setTimeout(() => {
-      // 创建窗格
-      createPanelWindow();
-
-      // 启动实况
-      startLive();
     }, 1000);
   } catch (error) {
     console.error("Login failed:", error);
   }
-  // onMounted end----------------
+
+  await getCameraData();
+  forCameraData();
+  await nextTick(() => {
+      // DOM 全部加载后的操作
+      const videoDOM = document.getElementById('videoDOM');
+      videoDOM ? videoDOM.style.overflow = 'hidden' : ''  // 这里可以获取到 videoDOM 的 DOM 结构
+      console.log('DOM 结构',videoDOM); // 这里可以获取到 videoDOM 的 DOM 结构
+    });
+
 });
 
 // 初始化
@@ -140,14 +144,12 @@ const init = async (initData: any) => {
     .init(initData)
     .then(function (res: any) {
       if (res.ErrCode === 0) {
-        console.log("登陆成功res", res);
         checkInit(); // 检查初始化状态
       } else {
-        // alert(res.ErrMsg);
+        console.error(res.ErrMsg);
       }
     })
     .catch(function (err: any) {
-      // alert("调用失败");
       console.error(err);
     });
 };
@@ -158,11 +160,9 @@ const loadScript = () => {
     const script = document.createElement("script");
     script.src = "http://10.141.10.10:8093/static/imosPlayer.min.js";
     script.onload = () => {
-      console.log("imosPlayer.min.js loaded successfully");
       resolve();
     };
     script.onerror = () => {
-      console.error("Failed to load imosPlayer.min.js");
       reject();
     };
     document.head.appendChild(script);
@@ -171,29 +171,115 @@ const loadScript = () => {
 
 // 创建窗格
 const createPanelWindow = () => {
-  let videoDom = (window as any).imosPlayer.createPanelWindow();
-  // 将 win 的内容赋值给 winContent
-  videoDom.style.width = "150px";
-  videoDom.style.height = "150px";
+  return new Promise((resolve, reject) => {
+    let videoDom = (window as any).imosPlayer.createPanelWindow();
+    videoDom.style.width = "140px";
+    videoDom.style.height = "160px";
+    videoDom.style.marginTop = "0.08rem";
+    videoDom.style.overflow = "hidden"; // 确保子元素内部也隐藏溢出部分
 
-  videoDomId.value = videoDom.id;
-  console.log("videoDomId", videoDomId.value);
-  // document.body.appendChild(videoDom);
-  const videoContainer = document.querySelector("#videoDOM");
-  videoContainer ? videoContainer.appendChild(videoDom) : null;
+    const videoContainer = document.querySelector("#videoDOM");
+    if (videoContainer) {
+      videoContainer.appendChild(videoDom);
+      resolve(videoDom.id);
+    } else {
+      reject("视频容器未找到");
+    }
+  });
+};
+
+const forCameraData = () => {
+  console.log('cameraListChecked222 ',cameraListChecked.value);
+  
+  cameraListChecked.value.forEach((item: any) => {
+    createPanelWindow().then((videoDomId: any) => {
+      let itemData = {
+        id: videoDomId,
+        ResCode: item.ResItemV1.ResCode,
+        ResName: item.ResItemV1.ResName,
+      };
+
+      startLive(itemData);
+    });
+  });
 };
 
 // 启动实况
-const startLive = () => {
+const startLive = (itemData: any) => {
   (window as any).imosPlayer
-    .playLive(videoDomId.value, {
-      camera: "31018900001320000109",
-      title: "虹桥枢纽运行管理一体化平台",
+    .playLive(itemData.id, {
+      camera: itemData.ResCode,
+      title: itemData.ResName,
       stream: 0,
     })
     .then((res: any) => {
-      console.log("实况", res);
+      // console.log("实况", res);
     });
+};
+
+// 获取相机数据
+const getCameraData = async () => {
+  let ipaddr = "http://10.141.10.10:8088/VIID/query";
+  let data1 = {
+    org: "iccsid",
+    condition: {
+      ItemNum: 3,
+      Condition: [
+        {
+          QueryType: 256,
+          LogicFlag: 0,
+          QueryData: "1001",
+        },
+        {
+          QueryType: 257,
+          LogicFlag: 0,
+          QueryData: "1",
+        },
+        {
+          QueryType: 1,
+          LogicFlag: 5,
+          QueryData: "",
+        },
+      ],
+      QueryCount: 1,
+      PageFirstRowNumber: 0,
+      PageRowNum: 200,
+    },
+  };
+  let conditionEncodeStr1 = encodeURIComponent(JSON.stringify(data1.condition));
+  let url = `${ipaddr}?org=${data1.org}&condition=${conditionEncodeStr1}`;
+  const res = await axios.get(url, {
+    headers: {
+      Authorization: AccessToken.value,
+    },
+  });
+  cameraListTotal.value = res.data.Result.InfoList;
+  console.log("相机总资源", cameraListTotal.value);
+  cameraListChecked.value = cameraListTotal.value.filter((item: any) => {
+    return item.OrgName.includes("B1");
+  });
+
+  // 视频暂时只显示4个，视频会盖住其他dom元素 暂时没法解决
+  cameraListChecked.value.length >= 4 ? cameraListChecked.value = cameraListChecked.value.slice(0, 4) : null;
+  console.log("Checked111---", cameraListChecked.value);
+};
+
+// 勾选数据
+const handleSelectChange = (value: any) => {
+  console.log('value',value);
+  const cameraTemp:any = []
+  cameraListTotal.value.forEach((item: any) => {
+    // console.log('item',item);
+    
+    if (item.OrgName.includes(value)) {
+      cameraTemp.push(item)
+    }
+  });
+  cameraListChecked.value = cameraTemp;
+  console.log('cameraListChecked',cameraListChecked.value);
+  
+  createPanelWindow()
+  forCameraData()
 };
 
 // 设置激活的 span
@@ -204,7 +290,6 @@ const setActive = (value: string) => {
 const checkInit = () => {
   let res = (window as any).imosPlayer.checkInit();
   console.log(res ? "已初始化" : "未初始化");
-  // alert(res ? "已初始化" : "未初始化");
 };
 
 onBeforeUnmount(() => {});
@@ -218,41 +303,45 @@ onBeforeUnmount(() => {});
   position: relative;
   .content {
     height: calc(100vh - 2.8rem);
-    overflow: auto;
     text-align: center;
     position: relative;
-    :deep(.ant-space) {
-      width: 100%;
-      .ant-space-item {
-        border: 1px solid #ccc;
-        width: calc(50% - 5px);
-        padding-top: 50%;
-        box-sizing: border-box;
-        position: relative;
-        > div {
-          width: 100%;
-          height: 100%;
-          position: absolute;
-          top: 0;
-          left: 0;
-          img {
-            display: inline-block;
-            width: 100%;
-            height: 100%;
-          }
-        }
-      }
-    }
+    // background-color: #fff;
+    overflow: hidden !important;
+    box-sizing: border-box;
+    //  overflow-y: scroll!important;
   }
 }
+
 .rowClass {
   display: flex;
 }
+
 .active {
   background-color: #035bcd;
   padding: 0.02rem;
   text-align: center;
   box-sizing: border-box;
   border-radius: 0.05rem;
+}
+
+#videoDOM {
+  display: flex;
+  flex-wrap: wrap;
+  // height: 5rem; /* 使用百分比高度 */
+  justify-content: space-between;
+  align-items: flex-start; /* 确保子元素从顶部开始排列 */
+  // overflow: hidden !important; /* 使用 !important 强制生效 */
+  // overflow-y: scroll!important;
+  box-sizing: border-box;
+  overflow: hidden;
+}
+.mapPic{
+  // width: 2rem;
+  // height: 2rem;
+  margin-top: 0.5rem;
+  img{
+    width: 100%;
+    height: 100%;
+  }
 }
 </style>
