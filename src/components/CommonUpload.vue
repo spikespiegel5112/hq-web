ee7760ced84a2936d54b23b0e10e3c82aae35de2
wangbolun <583288277@qq.com>
2025-01-09 13:44:45 +0800

    temp

--- src/components/CommonUpload.vue
+++ src/components/CommonUpload.vue
@@ -21,27 +21,22 @@
       <template #itemRender="{ file, actions }">
         <div class="filelist_wrapper">
           <div v-if="global.$checkFileType(file.name) === 'image'">
-            <a-image
-              v-if="file.createBy === 'uniview'"
-              width="1rem"
-              height="1rem"
-              :src="file.attachmentPath"
-            />
-            <a-image
-              v-else
-              :width="200"
-              :src="global.$getImageUrl(file.name)"
-            />
+            <a-image width="1rem" height="1rem" :src="getImgUrl(file)" />
+            <a-button
+              v-if="!checkUniView(file)"
+              class="deletebutton"
+              type="link"
+              @click="actions.remove"
+            >
+              <DeleteOutlined />
+            </a-button>
+          </div>
+          <div v-else-if="global.$checkFileType(file.name) === 'file'">
+            <FileOutlined />
             <a-button class="deletebutton" type="link" @click="actions.remove">
               <DeleteOutlined />
             </a-button>
           </div>
-          <template v-else-if="global.$checkFileType(file.name) === 'file'">
-            aaaa
-            <a-button class="deletebutton" type="link" @click="actions.remove">
-              <DeleteOutlined />
-            </a-button>
-          </template>
         </div>
       </template>
     </a-upload>
@@ -61,7 +56,11 @@ import {
   nextTick,
   defineEmits,
 } from "vue";
-import { UploadOutlined, DeleteOutlined } from "@ant-design/icons-vue";
+import {
+  UploadOutlined,
+  DeleteOutlined,
+  FileOutlined,
+} from "@ant-design/icons-vue";
 
 import { attachmentDeleteRequest } from "@/api/management";
 
@@ -110,9 +109,8 @@ watch(
         uid: index,
         name: item.attachmentName,
         status: "done",
-        deleteAble: checkUniView(item),
-        url: checkUniViewImage(item),
-        // url: `${baseUrl}/attachment/download?id=${item.id}`,
+        deleteAble: !checkUniView(item),
+        url: checkFileUrl(item),
       });
     });
   },
@@ -122,19 +120,45 @@ watch(
 );
 
 const checkUniView = (item: any) => {
-  return item.createBy !== "uniview";
+  return item.createBy === "uniview";
+};
+
+const getImgUrl = (item: any) => {
+  if (item.directLocation) {
+    return item.attachmentName;
+  } else {
+    return checkUniViewImage(item);
+  }
+};
+
+const checkFileUrl = (item: any) => {
+  let result: string;
+  if (checkUniView(item)) {
+    result = item.attachmentPath;
+  } else {
+    const fileType = global.$checkFileType(item.attachmentName);
+
+    switch (fileType) {
+      case "file":
+        result = global.$getImageUrl("/src/assets/file_icon.png");
+        break;
+      case "video":
+        result = global.$getImageUrl("/src/assets/file_icon.png");
+        break;
+      default:
+        result = `${global.$getBaseUrl()}/attachment/download?id=${item.id}`;
+        break;
+    }
+  }
+  return result;
 };
 
 const checkUniViewImage = (item: any) => {
   let result: string;
-  const baseUrl =
-    global.$store.state.app.envMode.MODE === "test"
-      ? "http://localhost:9009/manage"
-      : "";
-  if (checkUniView(item)) {
+  if (item.createBy === "uniview") {
     result = item.attachmentPath;
   } else {
-    result = `${baseUrl}/attachment/download?id=${item.id}`;
+    result = `${global.$getBaseUrl()}/attachment/download?id=${item.id}`;
   }
   return result;
 };
@@ -217,13 +241,42 @@ onMounted(() => {
       rgba(68, 121, 255, 0)
     );
   }
-  .filelist_wrapper {
-    > div {
+  :deep(.ant-upload-list) {
+    margin: 0.15rem 0 0 0;
+    .ant-upload-list-item-container {
       display: inline-block;
-      position: relative;
-      .deletebutton {
-        position: absolute;
-        right: 0;
+      margin: 0 0.2rem 0 0;
+      height: auto !important;
+      border: 1px solid #00266f;
+      border-radius: 0.1rem;
+      &:hover {
+        border: 1px solid #0059ff;
+        transition: 0.3s all;
+      }
+      .filelist_wrapper {
+        width: 1rem;
+        height: 1rem;
+        > div {
+          display: inline-flex;
+          width: 100%;
+          height: 100%;
+          position: relative;
+          align-items: center;
+          justify-content: center;
+          .anticon-file {
+            font-size: 0.5rem;
+          }
+          .deletebutton {
+            height: 20px;
+            padding: 0;
+            position: absolute;
+            right: 0;
+            top: 0;
+            .anticon {
+              font-size: 0.2rem;
+            }
+          }
+        }
       }
     }
   }
